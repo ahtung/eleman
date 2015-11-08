41dd5fe4ea01e00478cfef073134ba1b39c917f1
Onur Kucukkece <onurkucukkece@gmail.com>
2015-11-08 19:28:50 +0200

    reduced allowed content types

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,6 +2,6 @@ class Setting < ActiveRecord::Base
   belongs_to :organization
   has_attached_file :file
   validates_attachment :file, presence: true,
-    content_type: { content_type: ['text/yaml', 'application/x-yaml', 'text/x-yaml', 'application/yaml', 'text/plain'] },
+    content_type: { content_type: ['text/yaml', 'text/x-yaml'] },
     size: { in: 0..10.megabytes }
 end
