514490ff0bfe5f377df57548a2e7dec34289fc9b
Onur Kucukkece <onurkucukkece@gmail.com>
2015-11-08 18:34:41 +0200

    added size validation

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,4 +3,5 @@ class Setting < ActiveRecord::Base
   has_attached_file :file
   validates_attachment :file, presence: true,
     content_type: { content_type: "text/yaml" },
+    size: { in: 0..10.megabytes }
 end
