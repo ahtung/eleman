e2348a128909784663675f78824dd4debc8aeaa2
Onur Kucukkece <onurkucukkece@gmail.com>
2015-11-08 18:15:26 +0200

    added validation

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,4 +1,6 @@
 class Setting < ActiveRecord::Base
   belongs_to :organization
   has_attached_file :file
+  validates_attachment :file, presence: true,
+    content_type: { content_type: "text/yaml" },
 end
