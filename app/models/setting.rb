2cb7a908d42b37675de8894a9e5561a41dcef77d
Onur Kucukkece <onurkucukkece@gmail.com>
2015-11-08 19:10:53 +0200

    content type increased

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,6 +2,6 @@ class Setting < ActiveRecord::Base
   belongs_to :organization
   has_attached_file :file
   validates_attachment :file, presence: true,
-    content_type: { content_type: "text/yaml" },
+    content_type: { content_type: ['text/yaml', 'application/x-yaml', 'text/x-yaml', 'application/yaml', 'text/plain'] },
     size: { in: 0..10.megabytes }
 end
